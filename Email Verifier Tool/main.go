d8aee0bd543503b301a4cb2ff364762e5204566e
UtkarshUmre <112888849+UtkarshUmre@users.noreply.github.com>
2024-03-01 22:11:21 +0530

    Update main.go

--- Email Verifier Tool/main.go
+++ Email Verifier Tool/main.go
@@ -2,11 +2,18 @@ package main
 
 import ("fmt"
         "bufio"
-		"log"
-		"os"
+        "log"
+        "os"
 )
 
 func main(){
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord\n")
+
+	for scanner.scan(){
+		checkDomain(scanner.Text())
+		}
+	if err := scanner.Err(); err != nil {
+		log.Fatal ( "Error: could not read from input : %v\n ", err) 
+		}
 }
